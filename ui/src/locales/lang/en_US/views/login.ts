9709178e5091910cbefdf1562687967d3cee0b45
Dale <dale@DaledeMacBook-Pro.local>
2024-07-06 08:50:16 +0800

    add the multiplanguage support for login page

---
+++ ui/src/locales/lang/en_US/views/login.ts
@@ -0,0 +1,10 @@
+export default {
+  login:{
+    "welcomeMessage": "Welcome to StarCloudKB Intelligent Knowledge Base",
+    "normalLogin": "Normal Login",
+    "loginButton": "Login",
+    "forgotPassword": "Forgot Password?",
+    "requiredUsername": "Please enter your username",
+    "requiredPassword": "Please enter your password"
+  }
+}
