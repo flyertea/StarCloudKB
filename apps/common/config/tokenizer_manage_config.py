1144973dabd4db63906883c9ecedac911ddac756
dale <dale@flyertea.com>
2024-07-09 21:23:57 +0800

    更换token到bert

--- apps/common/config/tokenizer_manage_config.py
+++ apps/common/config/tokenizer_manage_config.py
@@ -7,18 +7,39 @@
     @desc:
 """
 
+from transformers import BertTokenizerFast
 
 class TokenizerManage:
     tokenizer = None
 
     @staticmethod
     def get_tokenizer():
-        from transformers import GPT2TokenizerFast
         if TokenizerManage.tokenizer is None:
-            TokenizerManage.tokenizer = GPT2TokenizerFast.from_pretrained(
-                'gpt2',
-                cache_dir="/opt/maxkb/model/tokenizer",
-                local_files_only=True,
-                resume_download=False,
-                force_download=False)
+            try:
+                TokenizerManage.tokenizer = BertTokenizerFast.from_pretrained(
+                    'hfl/chinese-roberta-wwm-ext',
+                    cache_dir="/opt/maxkb/model/tokenizer",
+                    local_files_only=True,
+                    resume_download=False,
+                    force_download=False
+                )
+            except Exception as e:
+                print(f"Error loading tokenizer: {e}")
+                raise
         return TokenizerManage.tokenizer
+#
+#
+# class TokenizerManage:
+#     tokenizer = None
+#
+#     @staticmethod
+#     def get_tokenizer():
+#         from transformers import GPT2TokenizerFast
+#         if TokenizerManage.tokenizer is None:
+#             TokenizerManage.tokenizer = GPT2TokenizerFast.from_pretrained(
+#                 'openai-community/gpt2',
+#                 cache_dir="/opt/maxkb/model/tokenizer",
+#                 local_files_only=True,
+#                 resume_download=False,
+#                 force_download=False)
+#         return TokenizerManage.tokenizer
