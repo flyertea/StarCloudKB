7aca5c8c403f69f72c66d3e209bc2a486450d2a2
dale <dale@flyertea.com>
2024-07-10 04:01:34 +0800

    poetry 转 pip requirement

--- apps/manage.py
+++ apps/manage.py
@@ -7,6 +7,9 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartdoc.settings')
+    os.environ['HF_HOME'] = '/opt/maxkb/model/base'
+    os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
